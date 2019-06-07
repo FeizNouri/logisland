645609af3c36aaf24d852da7a2fdbeee07f868c9
FeizNouri <feiznouri@gmail.com>
2019-06-07 02:34:15 +0100

    added the possibility to either encrypt or decrypt one or several fields of a record, encrypt: any type->bytes, decrypt: bytes->Object

--- logisland-components/logisland-processors/logisland-processor-common/src/main/java/com/hurence/logisland/processor/EncryptField.java
+++ logisland-components/logisland-processors/logisland-processor-common/src/main/java/com/hurence/logisland/processor/EncryptField.java
@@ -1,9 +1,12 @@
 package com.hurence.logisland.processor;
 
+import com.hurence.logisland.annotation.behavior.DynamicProperty;
+import com.hurence.logisland.component.AllowableValue;
 import com.hurence.logisland.component.PropertyDescriptor;
 import com.hurence.logisland.record.Field;
 import com.hurence.logisland.record.FieldType;
 import com.hurence.logisland.record.Record;
+import com.hurence.logisland.validator.StandardValidators;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import sun.misc.BASE64Decoder;
@@ -17,11 +20,21 @@ import java.io.UnsupportedEncodingException;
 import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.spec.KeySpec;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.text.ParseException;
+import java.util.*;
 import java.util.logging.Level;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+
+@DynamicProperty(name = "field to encrypt",
+        supportsExpressionLanguage = true,
+        value = "a default value",
+        description = "encrypt the field value")
 
 
 public class EncryptField extends AbstractProcessor {
@@ -59,6 +72,10 @@ public class EncryptField extends AbstractProcessor {
             .defaultValue("azerty1234567890")
             .build();
 
+
+
+    private Collection<String> fieldTypes = null;
+
     @Override
     public void init(final ProcessContext context) {
         List<PropertyDescriptor> properties = new ArrayList<>();
@@ -67,6 +84,7 @@ public class EncryptField extends AbstractProcessor {
         properties.add(KEY);
 
         properties = Collections.unmodifiableList(properties);
+        fieldTypes = getFieldsNameMapping(context);
     }
 
     @Override
@@ -80,10 +98,25 @@ public class EncryptField extends AbstractProcessor {
 
     }
 
+    @Override
+    protected PropertyDescriptor getSupportedDynamicPropertyDescriptor(final String propertyDescriptorName) {
+        return new PropertyDescriptor.Builder()
+                .name(propertyDescriptorName)
+                .expressionLanguageSupported(false)  // TODO understand what expressionLanguage is !!!
+                .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+                .required(false)
+                .dynamic(true)
+                .build();
+    }
+
+    // check if the algorithm chosen is AES, otherwaie it is DES
     public static boolean isAESAlgorithm(final String algorithm) {
         return algorithm.startsWith("A");
     }
 
+    // encrpyt or decript data with AES algo
+    //if encrypt: input object / output byte[]
+    //if decrypt: input field (the field will be in type FieldType.BYTES) / output object
     public class ExempleAES {
 
         private static final String ALGO ="AES";
@@ -93,22 +126,21 @@ public class EncryptField extends AbstractProcessor {
             keyValue = key.getBytes();
         }
 
-        public String encrypt (String Data) throws Exception{
+        public byte[] encrypt (Object Data) throws Exception{
             Key key = generateKey();
             Cipher c = Cipher.getInstance(ALGO);
             c.init(Cipher.ENCRYPT_MODE, key);
-            byte[] encVal = c.doFinal(Data.getBytes());
-            String encryptedValue = new BASE64Encoder().encode(encVal);
-            return  encryptedValue;
+            byte[] encVal = c.doFinal(toByteArray(Data));
+            return  encVal;
         }
 
-        public String decrypt (String encryptedData) throws  Exception {
+        public Object decrypt (Field encryptedData) throws  Exception {
             Key key = generateKey();
             Cipher c = Cipher.getInstance(ALGO);
             c.init(Cipher.DECRYPT_MODE, key);
-            byte[] decodedValue = new BASE64Decoder().decodeBuffer(encryptedData);
-            byte[] decValue = c.doFinal(decodedValue);
-            String decryptedValue = new String(decValue);
+            byte[] encryptedDataBytes = toByteArray(encryptedData);
+            byte[] decValue = c.doFinal(encryptedDataBytes);
+            Object decryptedValue = toObject(decValue);
             return decryptedValue;
         }
 
@@ -119,7 +151,9 @@ public class EncryptField extends AbstractProcessor {
 
     }
 
-
+    // encrpyt or decript data with DES algo
+    //if encrypt: input object / output byte[]
+    //if decrypt: input field (the field will be in type FieldType.BYTES) / output object
     public class ExempleDES {
 
         private static final String UNICODE_FORMAT = "UTF8";
@@ -142,43 +176,33 @@ public class EncryptField extends AbstractProcessor {
             key = mySecretKeyFactory.generateSecret(myKeySpec);
         }
 
-        public String encrypt (String unencryptedString) {
-            String encryptedString = null;
+        public byte[] encrypt (Object unencryptedString) {
+            byte[] encryptedText = null;
             try {
                 cipher.init(Cipher.ENCRYPT_MODE, key);
-                byte[] plainText = unencryptedString.getBytes(UNICODE_FORMAT);
-                byte[] encryptedText = cipher.doFinal(plainText);
-                BASE64Encoder base64encoder = new BASE64Encoder();
-                encryptedString= base64encoder.encode(encryptedText);
-            } catch (InvalidKeyException | UnsupportedEncodingException | IllegalBlockSizeException | BadPaddingException e) {
+                byte[] plainText = toByteArray(unencryptedString);
+                encryptedText = cipher.doFinal(plainText);
+            } catch (InvalidKeyException | IllegalBlockSizeException | BadPaddingException | IOException  e) {
             }
-            return encryptedString;
+            return encryptedText;
         }
 
-        public String decrypt (String encryptedString) {
-            String decryptedText = null;
+        public Object decrypt (Field encryptedString) {
+            Object decryptedText = null;
             try{
                 cipher.init(Cipher.DECRYPT_MODE, key);
-                BASE64Decoder base64decoder = new BASE64Decoder();
-                byte[] encrypedText = base64decoder.decodeBuffer(encryptedString);
-                byte[] plainText = cipher.doFinal(encrypedText);
-                decryptedText = bytes2String(plainText);
+                byte[] encryptedStringBytes = toByteArray(encryptedString);
+                byte[] plainText = cipher.doFinal(encryptedStringBytes);
+                decryptedText = toObject(plainText);
 
-            } catch (InvalidKeyException | IOException | IllegalBlockSizeException | BadPaddingException e) {
+            } catch (InvalidKeyException | IOException | IllegalBlockSizeException | BadPaddingException | ClassNotFoundException e) {
             }
             return decryptedText;
         }
 
-        private String bytes2String(byte[] bytes) {
-            StringBuilder stringBuffer = new StringBuilder();
-            for (int i=0; i < bytes.length; i++) {
-                stringBuffer.append((char) bytes[i]);
-            }
-            return stringBuffer.toString();
-        }
     }
 
-    
+
 
 
     @Override
@@ -191,34 +215,98 @@ public class EncryptField extends AbstractProcessor {
         }
 
         try {
-            Collection<Field> allfieldsToEncrypt;
+            Collection<Field> allfieldsToEncrypt = null;
+            Collection<String> allfieldsToEncrypt_InString ;
 
             for (Record record : records) {
-                allfieldsToEncrypt = record.getAllFields();
+                // check if user choose some specific field or fields to encrypt, if don't we'll encrypt all fields.
+                if (getFieldsNameMapping(context) == null) {
+                    allfieldsToEncrypt = record.getAllFields();
+                } else {
+                    allfieldsToEncrypt_InString = getFieldsNameMapping(context);
+                    for (String name : allfieldsToEncrypt_InString) {
+                        allfieldsToEncrypt.add(record.getField(name));
+                        /*final Object inputDateValue = context.getPropertyValue(name).evaluate(record);*/ // idea ! : we can take objects as input not fields!
+                    }
+
+                }
 
                 for (Field field : allfieldsToEncrypt) {
 
                     if (isAESAlgorithm(context.getProperty(ALGO))) {
                         ExempleAES encryptAES = new ExempleAES(context.getProperty(KEY));
                         if (encrypt) {
-                            record.setField(field.getName(), FieldType.STRING, encryptAES.encrypt(field.asString()));
+                            record.setField(field.getName(), FieldType.BYTES, encryptAES.encrypt(field)); // is field an Object ??!!
                         } else {
-                            record.setField(field.getName(), FieldType.STRING, encryptAES.decrypt(field.asString()));
+                            record.setField(field.getName(), field.getType(), encryptAES.decrypt(field)); // !!!!!!!!!!! how to know the original type of the field before encrypting
                         }
 
                     } else {
                         ExempleDES encryptDES = new ExempleDES(context.getProperty(KEY));
                         if (encrypt) {
-                            record.setField(field.getName(), FieldType.STRING, encryptDES.encrypt(field.asString()));
+                            record.setField(field.getName(), FieldType.BYTES, encryptDES.encrypt(field));
                         } else {
-                            record.setField(field.getName(), FieldType.STRING, encryptDES.decrypt(field.asString()));
+                            record.setField(field.getName(), FieldType.STRING, encryptDES.decrypt(field));
                         }
                     }
                 }
+
             }
         } catch (Throwable t) {
             logger.error("error while setting id for records", t);
         }
         return records;
+    }
+    // convert objects to byte array
+    public static byte[] toByteArray(Object obj) throws IOException {
+        byte[] bytes ;
+        ByteArrayOutputStream bos = null;
+        ObjectOutputStream oos = null;
+        try {
+            bos = new ByteArrayOutputStream();
+            oos = new ObjectOutputStream(bos);
+            oos.writeObject(obj);
+            oos.flush();
+            bytes = bos.toByteArray();
+        } finally {
+            if (oos != null) {
+                oos.close();
+            }
+            if (bos != null) {
+                bos.close();
+            }
+        }
+        return bytes;
+    }
+    // convert byte array to object
+    public static Object toObject(byte[] bytes) throws IOException, ClassNotFoundException {
+        Object obj = null;
+        ByteArrayInputStream bis = null;
+        ObjectInputStream ois = null;
+        try {
+            bis = new ByteArrayInputStream(bytes);
+            ois = new ObjectInputStream(bis);
+            obj = ois.readObject();
+        } finally {
+            if (bis != null) {
+                bis.close();
+            }
+            if (ois != null) {
+                ois.close();
+            }
+        }
+        return obj;
+    }
+    // get the specific field or fields to encrypt form the dynamic property
+    private Collection<String> getFieldsNameMapping(ProcessContext context) {
+        Collection<String> fieldsNameMappings = null;
+        for (final Map.Entry<PropertyDescriptor, String> entry : context.getProperties().entrySet()) {
+            if (!entry.getKey().isDynamic()) {
+                continue;
+            }
+            final String fieldName = entry.getKey().getName();
+            fieldsNameMappings.add(fieldName);
+        }
+        return fieldsNameMappings;
     }
 }
